ece2ee9eecbf980bff39704ccdd5f8218e1f5540
Simon Watts <14802483+siwatts@users.noreply.github.com>
2024-12-02 16:40:21 +0000

    Refactored for part 2 - same wrong answer

--- day2/day2-part2.cc
+++ day2/day2-part2.cc
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <fstream>
-#include <algorithm> // For sort()
+#include <algorithm>
 #include <vector>
 
 using namespace std;
+
+bool isLevelSafe(vector<int> level);
 
 int main(int argc, char* argv[])
 {
@@ -11,7 +13,7 @@ int main(int argc, char* argv[])
     // For testing
     int debuglimit = 1;
     int debug = 0;
-    bool debugapply = true;
+    bool debugapply = false;
     if (debugapply) { cout << "DEBUG MODE : ON\nLINE LIMIT : " << debuglimit << "\n--" << endl; }
 
     // Input file
@@ -61,21 +63,66 @@ int main(int argc, char* argv[])
         // Make a sorted copy of the vector so we don't modify the original
         vector<int> numSort = num;
         sort(numSort.begin(), numSort.end());
+        int removedItems;
+        vector<vector<int>> listValidSubArrays;
 
         // If they match, we are ascending
+        isAsc = false;
         if (num == numSort) {
-            //cout << "Vectors are identical" << endl;
+            removedItems = 0;
             isAsc = true;
         }
         else {
-            isAsc = false;
+            // Part 2: Tolerate 1 incorrect element, try removing each in turn
+            // to see if we still end up with valid sub-arrays
+            // The logic around which of the possible elements should be removed is difficult
+            // to know in advance when combined with the 2nd part of part 2 involving
+            // the differences between elements. So just store them all to try later
+            for (size_t i = 0; i < num.size(); i++) {
+                vector<int> subList = num;
+                vector<int> subSorted = numSort;
+                auto sortedIndex = find(subSorted.begin(), subSorted.end(), num[i]);
+                if (sortedIndex == subSorted.end())
+                    cout << "ERROR: Didn't find the element we're about to remove in the sorted copy array, something went badly wrong!\n";
+                subList.erase(subList.begin() + i);
+                subSorted.erase(sortedIndex);
+                // Check validity
+                if (subList == subSorted) {
+                    //cout << "Found a valid sublist by removing element " << i << " of line " << debug << endl;
+                    listValidSubArrays.push_back(subList);
+                    removedItems = 1;
+                    isAsc = true;
+                }
+            }
+        }
+
+        // If not Asc, try Desc
+        isDesc = false;
+        if (!isAsc) {
             // May be descending instead, check by reversing sorted array
             reverse(numSort.begin(), numSort.end());
             if (num == numSort) {
+                removedItems = 0;
                 isDesc = true;
             }
             else {
-                isDesc = false;
+                // Try removing items, as above
+                for (size_t i = 0; i < num.size(); i++) {
+                    vector<int> subList = num;
+                    vector<int> subSorted = numSort;
+                    auto sortedIndex = find(subSorted.begin(), subSorted.end(), num[i]);
+                    if (sortedIndex == subSorted.end())
+                        cout << "ERROR: Didn't find the element we're about to remove in the sorted copy array, something went badly wrong!\n";
+                    subList.erase(subList.begin() + i);
+                    subSorted.erase(sortedIndex);
+                    // Check validity
+                    if (subList == subSorted) {
+                        //cout << "Found a valid sublist by removing element " << i << " of line " << debug << endl;
+                        listValidSubArrays.push_back(subList);
+                        removedItems = 1;
+                        isDesc = true;
+                    }
+                }
             }
         }
 
@@ -83,16 +130,19 @@ int main(int argc, char* argv[])
         //for (int i = 0; i < num.size(); i++) {
         //    cout << num[i] << endl;
         //}
-        //cout << "Vector copy:\n";
-        //for (int i = 0; i < numSort.size(); i++) {
-        //    cout << numSort[i] << endl;
-        //}
 
-        // Check that difference between successive values is no more than 3, and not 0
-        safe = isAsc || isDesc;
-        for (size_t i = 1; safe && i < num.size(); i++) {
-            if (abs(num[i] - num[i-1]) == 0 || abs(num[i] - num[i-1]) > 3) {
+        if (isAsc || isDesc) {
+            if (removedItems == 0) {
+                // We didn't remove any items, so we can check our original vector
+                safe = isLevelSafe(num);
+            }
+            else if (removedItems == 1 && listValidSubArrays.size() > 0) {
+                // We have some valid sub arrays to check
                 safe = false;
+                for (size_t i = 0; i < listValidSubArrays.size(); i++) {
+                    if (isLevelSafe(listValidSubArrays[i]))
+                        safe = true;
+                }
             }
         }
 
@@ -110,5 +160,19 @@ int main(int argc, char* argv[])
 
     cout << "--\nEnd.\n";
     return 0;
+}
+
+bool isLevelSafe(vector<int> level)
+{
+    // Check that difference between successive values is no more than 3, and not 0
+    // This function assumes that the array is already valid and sorted (ie. ascending
+    // or descending order)
+    bool safe = true;
+    for (size_t i = 1; safe && i < level.size(); i++) {
+        if (abs(level[i] - level[i-1]) == 0 || abs(level[i] - level[i-1]) > 3) {
+            safe = false;
+        }
+    }
+    return safe;
 }
 
