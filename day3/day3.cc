bf905564ad8e691b68b3ee3124f1b9b47617e7cc
Simon Watts <14802483+siwatts@users.noreply.github.com>
2024-12-03 19:18:36 +0000

    Broken part 2

--- day3/day3.cc
+++ day3/day3.cc
@@ -2,7 +2,8 @@
 #include <fstream>
 //#include <algorithm>
 #include <vector>
-#include <regex>
+//#include <regex>
+#include <queue>
 
 using namespace std;
 
@@ -14,7 +15,7 @@ int main(int argc, char* argv[])
     // For testing
     int debuglimit = 1;
     int debug = 0;
-    bool debugapply = false;
+    bool debugapply = true;
     if (debugapply) { cout << "DEBUG MODE : ON\nLINE LIMIT : " << debuglimit << "\n--" << endl; }
 
     // TODO: Add debug flag detection from CLI, and check whether file exists
@@ -36,47 +37,118 @@ int main(int argc, char* argv[])
     size_t pos;
     //regex searchpattern("mul\\(\\d+,\\d+\\)");
     //smatch match;
+    // TODO: Disable may cross line boundaries, be prepared to check status at the
+    // end of this while loop and log it for the next iteration to start out disabled
     while (getline(input, line) && (!debugapply || debug < debuglimit))
     {
         debug++;
         //cout << line << "\n--" << endl;
 
-        // TODO: Try regex
-        // Looking for exact pattern 'mul(X,Y)'
-        // while (regex_search(line, match, searchpattern)) {
-        //     for (auto x : match) {
-        //         cout << "Regex found match '" << x << "'\n";
-        //     }
-        //     searchpattern = match.suffix().str();
-        // }
+        // Part 2
+        // Look for "do()" and "don't()" strings in the text
+        // A line starts out true by default, but after a "don't()" string
+        // any mulitplications are ignored until a "do()" string occurs
+        pos = line.find("don't()");
+        queue<int> disablePos;
+        queue<int> enablePos;
+        if (pos != string::npos) {
+            // Found a deactivation, so we need to log everything
+            while (pos != string::npos) {
+                disablePos.push(pos);
+                cout << "Logged a don't() string at line " << debug << " pos " << pos << endl;
+                pos = line.find("don't()", pos+1);
+            }
+            pos = 0;
+            do {
+                enablePos.push(pos);
+                cout << "Logged a do() string at line " << debug << " pos " << pos << endl;
+                pos = line.find("do()", pos+1);
+            } while (pos != string::npos);
+        }
+        // Find disable zones
+        queue<pair<int,int>> disableZones;
+        while (!disablePos.empty()) {
+            int start = disablePos.front();
+            disablePos.pop();
+            // We are now in a disable zone
+            // Find end, if any
+            int end = start;
+            while (!enablePos.empty() && end <= start) {
+                end = enablePos.front();
+                enablePos.pop();
+            }
+            if (end == start) {
+                // Didn't find an end, so set to end of line
+                // TODO: This may not be the correct thing to do for the solution answer!
+                end = line.length();
+            }
+            pair<int,int> zone = {start, end};
+            disableZones.push(zone);
+            cout << "Found disable zone from start: " << start << " to end: " << end << endl;
+            // Need to discard any following disables that are in this interval before continuing
+            // as they happen in a region that is already disabled
+            start = disablePos.front();
+            while (start <= end) {
+                // Discard it
+                disablePos.pop();
+                start = disablePos.front();
+            }
+        }
 
         // Search manually
         // For a starting 'm'
         pos = line.find_first_of("m");
         int offset = 0; // Skip parts of the string we already searched
-        while (pos != string::npos)
+        while (pos != string::npos && pos != line.length())
         {
             // Matched 'm'
             //cout << "Found starting 'm' line " << debug << " at pos. " << pos << endl;
             int start = pos;
             pos = line.find_first_of(")", start);
             if (pos != string::npos) {
-                // Potential match
-                // We don't care if it is valid or not, if invalid we just return 0
-                //cout << "Found potential closing ')' line " << debug << " at pos. " << pos << endl;
-                int end = pos + 1;
-                int result = process_match(line.substr(start,end-start));
-                sum += result;
-                //line = line.substr(end); // For next cycle of loop
+                // Part 2, we may be in an invalid region of the line
+                // Check this first
+                bool inDisabledZone;
+                while (!disableZones.empty() && disableZones.front().second < pos) {
+                    // Discard all zones that end before we start
+                    disableZones.pop();
+                }
+                if (disableZones.empty()) {
+                    inDisabledZone = false;
+                } else {
+                    inDisabledZone = ( disableZones.front().first < pos );
+                    // Don't need to check (disableZones.front().second > pos), because the while loop did it above
+                }
 
-                // Next match?
-                // Can do this here so it only happens if there's at least 1 ")" remaining in the line
-                // to save on checking pointless "m"s
-                if (result > 0)
-                    offset = end;
-                else
-                    offset = start + 1;
-                pos = line.find_first_of("m", offset);
+                if (inDisabledZone) {
+                    // Don't bother checking the string
+                    // We can just jump straight to the end of the disabled zone before
+                    // doing any more string searching
+                    offset = disableZones.front().second;
+                }
+                else {
+                    // Potential match
+                    // We don't care if it is valid or not, if invalid we just return 0
+                    //cout << "Found potential closing ')' line " << debug << " at pos. " << pos << endl;
+                    int end = pos + 1;
+                    int result = process_match(line.substr(start,end-start));
+                    sum += result;
+                    //line = line.substr(end); // For next cycle of loop
+
+                    // Next match?
+                    // Can do this here so it only happens if there's at least 1 ")" remaining in the line
+                    // to save on checking pointless "m"s
+                    if (result > 0)
+                        offset = end;
+                    else
+                        offset = start + 1;
+                }
+                if (offset != line.length()) {
+                    pos = line.find_first_of("m", offset);
+                }
+                else {
+                    pos = string::npos;
+                }
             }
         }
     }
