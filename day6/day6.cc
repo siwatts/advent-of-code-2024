f055e63fd36d8617c0887bf58cfdc6b4d32784d0
Simon Watts <14802483+siwatts@users.noreply.github.com>
2024-12-07 14:42:56 +0000

    Start day 6, broken classes

---
+++ day6/day6.cc
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+enum Direction {
+    UP = 0,
+    RIGHT,
+    DOWN,
+    LEFT
+};
+
+class Lab {
+    public:
+        vector<string> grid;
+        char getChar(int x, int y);
+        char updateChar(int x, int y, char newval);
+};
+class Guard {
+    public:
+        int xpos;
+        int ypos;
+        enum Direction dir;
+};
+
+int main(int argc, char* argv[])
+{
+    cout << "--\nAoC Day 6\n--\n";
+    // For testing
+    int debuglimit = 10;
+    int debug = 0;
+    bool debugapply = false;
+    if (debugapply) { cout << "DEBUG MODE : ON\nLINE LIMIT : " << debuglimit << "\n--" << endl; }
+
+    // TODO: Add debug flag detection from CLI, and check whether file exists
+    string filename = "input";
+    if (argc == 1) {
+        cout << "Assume default input file '" << filename << "'\n";
+    }
+    else if (argc > 1) {
+        filename = argv[1];
+        cout << "Taking CLI input file name '" << filename << "'\n";
+    }
+    // Input file
+    ifstream input(filename);
+
+    // Variables for output
+    int sum = 0;
+
+    // Read input into our data structures
+    string line;
+    int lineNr = 0;
+    Lab lab;
+    Guard guard;
+    while (getline(input, line) && (!debugapply || debug < debuglimit))
+    {
+        debug++;
+        if (debugapply) {
+            //cout << line << endl;
+        }
+
+        lab.grid.push_back(line);
+        auto pos = line.find("^");
+        if (pos != string::npos) {
+            cout << "Found guard in starting position [" << pos << "," << lineNr << "]\n";
+            guard.xpos = pos;
+            guard.ypos = lineNr;
+            guard.dir = UP;
+        }
+        lineNr++;
+    }
+    // Finished with input file
+    input.close();
+
+    // Processing
+    cout << "";
+    cout << "Guard location before: " << lab.getChar(guard.xpos, guard.ypos) << endl;
+    lab.updateChar(guard.xpos, guard.ypos, 'X');
+    cout << "Guard location after: " << lab.getChar(guard.xpos, guard.ypos) << endl;
+
+    // Output
+    cout << "--\n";
+    cout << "Sum = " << sum << endl;
+
+    cout << "--\nEnd.\n";
+    return 0;
+}
+
+char Lab::getChar(int x, int y)
+{
+    // Return char. at x,y pos. so we don't have to remember the vector<string> line structure
+    if (grid.size() >= y) {
+        ostringstream os;
+        os << "Position [" << x << "," << y << "] is out of bounds, in y direction";
+        throw std::runtime_error(os.str()); // TODO: Make our own Exception class
+    }
+    if (grid[0].length() >= x) {
+        ostringstream os;
+        os << "Position [" << x << "," << y << "] is out of bounds, in x direction";
+        throw std::runtime_error(os.str()); // TODO: Make our own Exception class
+    }
+    string line = grid[y];
+    return line[x];
+}
+
+char Lab::updateChar(int x, int y, char newval)
+{
+    // Return char. at x,y pos. so we don't have to remember the vector<string> line structure
+    if (y < 0 || y >= grid.size()) {
+        ostringstream os;
+        os << "Position [" << x << "," << y << "] is out of bounds, in y direction";
+        throw std::runtime_error(os.str()); // TODO: Make our own Exception class
+    }
+    if (x < 0 || x >= grid[0].length()) {
+        ostringstream os;
+        os << "Position [" << x << "," << y << "] is out of bounds, in x direction";
+        throw std::runtime_error(os.str()); // TODO: Make our own Exception class
+    }
+    grid[y][x] = newval;
+}
+
