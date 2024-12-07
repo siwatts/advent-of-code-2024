5e851723292c37163e8dca6b1f80945228137279
Simon Watts <14802483+siwatts@users.noreply.github.com>
2024-12-07 19:19:45 +0000

    Day 6 Part 2 wrong answer

--- day6/day6.cc
+++ day6/day6.cc
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <vector>
 #include <sstream>
+#include <queue>
 
 using namespace std;
 
@@ -28,6 +29,10 @@ class Guard {
         enum Direction dir;
         pair<int,int> getNextPos();
         void turnRight();
+        // For part 2
+        queue<pair<int,int>> vertexPoints;
+        vector<pair<int,int>> infiniteLoopPoints;
+        bool atPossibleInfiniteLoop();
 };
 
 int main(int argc, char* argv[])
@@ -53,6 +58,7 @@ int main(int argc, char* argv[])
 
     // Variables for output
     int sum = 0;
+    int infiniteLoopPoints = 0;
 
     // Read input into our data structures
     string line;
@@ -87,6 +93,8 @@ int main(int argc, char* argv[])
     // Log starting position before moving
     lab.updateChar(guard.xpos, guard.ypos, marker);
     sum++;
+    // And for part 2, make our starting point the 1st vertex for our path (subsequently it will be turning points)
+    guard.vertexPoints.push({guard.xpos, guard.ypos});
     // Loop
     int steps = 0; int turns = 0;
     bool escaped = false;
@@ -122,6 +130,10 @@ int main(int argc, char* argv[])
                 guard.xpos = xNext;
                 guard.ypos = yNext;
                 steps++;
+                if (guard.atPossibleInfiniteLoop()) {
+                    cout << "Guard position [" << guard.xpos << "," << guard.ypos << "] (line:" << guard.ypos+1 << ",col:" << guard.xpos+1 << ") is a possible infinite loop point\n";
+                    infiniteLoopPoints++;
+                }
             }
         }
     }
@@ -136,6 +148,7 @@ int main(int argc, char* argv[])
     // Output
     cout << "--\n";
     cout << "Sum unique spots visited = " << sum << endl;
+    cout << "Sum possible infinite loop points = " << infiniteLoopPoints << endl;
 
     cout << "--\nEnd.\n";
     return 0;
@@ -236,5 +249,91 @@ void Guard::turnRight()
         default:
             break;
     }
+
+    // Log for P2
+    vertexPoints.push({xpos, ypos});
+}
+
+bool Guard::atPossibleInfiniteLoop()
+{
+    // TODO: NOTE the place that may be the infinite loop blocker may not be allowed and should therefore be discounted,
+    // e.g. if out of bounds or has already been travelled in the past because that would alter our past route
+
+    // Part 2 stuff:
+    // We know we're at a possible infinite loop position if we are at the vertex that completes the 4th side of a rectangle
+    // AND 'o' is a possible spot for an obstacle (not out of bounds or already previously visited)
+    // We won't get into this function if 'o' is out of bounds
+    // ...#.........
+    // ...2-->--3#..
+    // ...|.....|...
+    // ...^.....v...
+    // ...|.....|...
+    // <-o+--<--4...
+    // ...|.....#...
+    // ...1.........
+    // .............
+
+    // So we need at least 4 to check if we cross the path from the 1st to the 2nd. If so we can place another turn there
+    if (vertexPoints.size() < 4) {
+        return false;
+    }
+    // Any more and we can drop the oldest ones
+    while (vertexPoints.size() > 4) {
+        vertexPoints.pop();
+    }
+    // Now we have exactly 4 vertices in our past
+    // We only care about the oldest one as it is (1) in the diagram
+    pair<int,int> firstVertex = vertexPoints.front();
+    int firstX = firstVertex.first;
+    int firstY = firstVertex.second;
+    // Depends on the direction we're travelling
+    switch (dir)
+    {
+        // Remember y increases as we go down the file! So higher y means BELOW us
+        case UP:
+            // 1st vertex will be left of us and equal y
+            if (firstX <= xpos && firstY == ypos) {
+                //cout << "Found infinite point passing vertex [" << firstX << "," << firstY << "] at guard position [" << xpos << "," << ypos << "] going UP\n";
+                return true;
+            }
+            else {
+                return false;
+            }
+            break;
+        case RIGHT:
+            // 1st vertex will be higher and equal x
+            if (firstX == xpos && firstY <= ypos) {
+                //cout << "Found infinite point passing vertex [" << firstX << "," << firstY << "] at guard position [" << xpos << "," << ypos << "] going RIGHT\n";
+                return true;
+            }
+            else {
+                return false;
+            }
+            break;
+        case DOWN:
+            // 1st vertex will be right of us and equal y
+            if (firstX >= xpos && firstY == ypos) {
+                //cout << "Found infinite point passing vertex [" << firstX << "," << firstY << "] at guard position [" << xpos << "," << ypos << "] going DOWN\n";
+                return true;
+            }
+            else {
+                return false;
+            }
+            break;
+        case LEFT:
+            // 1st vertex will be lower and equal x
+            if (firstX == xpos && firstY >= ypos) {
+                //cout << "Found infinite point passing vertex [" << firstX << "," << firstY << "] at guard position [" << xpos << "," << ypos << "] going LEFT\n";
+                return true;
+            }
+            else {
+                return false;
+            }
+            break;
+        default:
+            return false;
+            break;
+    }
+
 }
 
