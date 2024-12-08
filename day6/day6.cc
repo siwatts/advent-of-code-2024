41b858c6017f6dcc6d89c438914cebb692c6f4bc
Simon Watts <14802483+siwatts@users.noreply.github.com>
2024-12-08 02:57:00 +0000

    Brute force Day 6 P2 working, unoptimised

--- day6/day6.cc
+++ day6/day6.cc
@@ -27,19 +27,22 @@ class Guard {
         int xpos;
         int ypos;
         enum Direction dir;
-        pair<int,int> getNextPos();
+        pair<int,int> getNextPos(int multiplier);
         void turnRight();
         void move();
         // For part 2
         vector<pair<int,int>> infiniteLoopPoints;
         bool atPossibleInfiniteLoop(Lab lab, char marker);
+        Direction getTravelDirectionOfPoint();
+        Direction getTravelDirectionOfPoint(int x, int y);
+        void moveBackwards();
     private:
         unordered_map<string,Direction> travelDirections;
-        Direction getTravelDirectionOfPoint();
         void setTravelDirectionOfPoint();
 };
 
 string getFileCoordinatesString(int x, int y);
+bool runGuardInfiniteLoopCheck(Guard g, Lab l, int xPosObstacle, int yPosObstacle);
 
 int main(int argc, char* argv[])
 {
@@ -71,6 +74,7 @@ int main(int argc, char* argv[])
     int lineNr = 0;
     Lab lab;
     Guard guard;
+    Guard guardP2;
     while (getline(input, line) && (!debugapply || debug < debuglimit))
     {
         debug++;
@@ -88,6 +92,10 @@ int main(int argc, char* argv[])
             guard.xpos = pos;
             guard.ypos = lineNr;
             guard.dir = UP;
+            // Part 2 guard here also
+            guardP2.xpos = pos;
+            guardP2.ypos = lineNr;
+            guardP2.dir = UP;
         }
         lineNr++;
     }
@@ -99,12 +107,15 @@ int main(int argc, char* argv[])
     // Log starting position before moving
     lab.updateChar(guard.xpos, guard.ypos, marker);
     sum++;
+    // For part 2
+    // Just log every single cell we pass over so we can brute force test them later
+    queue<pair<int,int>> potentialObstacles;
     // Loop
     int steps = 0; int turns = 0;
     bool escaped = false;
     while (!escaped)
     {
-        pair<int,int> nextPos = guard.getNextPos();
+        pair<int,int> nextPos = guard.getNextPos(1);
         int xNext = nextPos.first;
         int yNext = nextPos.second;
         // Are we at the edge
@@ -125,17 +136,19 @@ int main(int argc, char* argv[])
                 turns++;
             }
             else {
-                bool newplace = nextChar != marker;
+                bool newplace = (nextChar != marker);
                 if (newplace) {
                     // Not been here before, count it
                     sum++;
                     lab.updateChar(xNext, yNext, marker);
+                    // Log it for P2
+                    potentialObstacles.push({xNext, yNext});
                 }
                 // Move guard, this also logs the previous location's direction of travel
                 guard.move();
                 steps++;
                 if (!newplace && guard.atPossibleInfiniteLoop(lab, marker)) {
-                    cout << "Guard position " << getFileCoordinatesString(guard.xpos,guard.ypos) << " is an infinite loop point\n";
+                    //cout << "Guard position " << getFileCoordinatesString(guard.xpos,guard.ypos) << " is an infinite loop point\n";
                     infiniteLoopPoints++;
                 }
             }
@@ -149,10 +162,35 @@ int main(int argc, char* argv[])
         }
     }
 
+    // Part 2:
+    // Brute force approach
+    // Re-run the above, but this time, pop the first obstacle first
+    // Just try every point along the path...
+    int sumP2 = 0;
+    int p2Runs = 0;
+    while (potentialObstacles.size() != 0) {
+        p2Runs++;
+        pair<int,int> posNewObstacle = potentialObstacles.front();
+        potentialObstacles.pop();
+        int xPosNewObstacle = posNewObstacle.first;
+        int yPosNewObstacle = posNewObstacle.second;
+
+        cout << "Simulating P2 guard " << p2Runs << "/" << potentialObstacles.size ()+ p2Runs;
+        cout << " with obstacle at " << getFileCoordinatesString(xPosNewObstacle, yPosNewObstacle) << "\n";
+
+        // Guard is copied by function call so we can re-use it
+        bool infiniteLoop = runGuardInfiniteLoopCheck(guardP2, lab, xPosNewObstacle, yPosNewObstacle);
+        if (infiniteLoop) {
+            cout << "-- Guard " << p2Runs << " found an infinite loop! --\n";
+            sumP2++;
+        }
+    }
+
     // Output
     cout << "--\n";
     cout << "Sum unique spots visited = " << sum << endl;
     cout << "Sum possible infinite loop points = " << infiniteLoopPoints << endl;
+    cout << "Sum P2 = " << sumP2 << endl;
 
     cout << "--\nEnd.\n";
     return 0;
@@ -191,7 +229,7 @@ void Lab::updateChar(int x, int y, char newval)
     grid[y][x] = newval;
 }
 
-pair<int,int> Guard::getNextPos()
+pair<int,int> Guard::getNextPos(int multiplier)
 {
     // Be careful, y position actually increases as we go _down_ the lines!
     pair<int,int> next;
@@ -199,18 +237,18 @@ pair<int,int> Guard::getNextPos()
     {
         case UP:
             next.first = xpos;
-            next.second = ypos - 1;
+            next.second = ypos - multiplier;
             break;
         case RIGHT:
-            next.first = xpos + 1;
+            next.first = xpos + multiplier;
             next.second = ypos;
             break;
         case DOWN:
             next.first = xpos;
-            next.second = ypos + 1;
+            next.second = ypos + multiplier;
             break;
         case LEFT:
-            next.first = xpos - 1;
+            next.first = xpos - multiplier;
             next.second = ypos;
             break;
         default:
@@ -236,7 +274,15 @@ void Guard::move() {
     // Before we move log the direction we moved on our previous spot for Part 2
     setTravelDirectionOfPoint();
     // Now move
-    pair<int,int> nextPos = getNextPos();
+    pair<int,int> nextPos = getNextPos(1);
+    xpos = nextPos.first;
+    ypos = nextPos.second;
+}
+
+void Guard::moveBackwards() {
+
+    // For part 2
+    pair<int,int> nextPos = getNextPos(-1);
     xpos = nextPos.first;
     ypos = nextPos.second;
 }
@@ -267,7 +313,7 @@ void Guard::turnRight()
 
 bool Guard::atPossibleInfiniteLoop(Lab lab, char marker)
 {
-    cout << "Checking previously travelled point " << getFileCoordinatesString(xpos, ypos) << " for infinite loop potential\n";
+    //cout << "Checking previously travelled point " << getFileCoordinatesString(xpos, ypos) << " for infinite loop potential\n";
     // Part 2 stuff:
     // Every time we cross a point we have already been to it could be a possible loop point
     // It depends on the direction we were travelling when we most recently crossed that point
@@ -298,7 +344,7 @@ bool Guard::atPossibleInfiniteLoop(Lab lab, char marker)
     }
 
     if (!possible) {
-        cout << "Point " << getFileCoordinatesString(xpos, ypos) << " found false after direction check\n";
+        //cout << "Point " << getFileCoordinatesString(xpos, ypos) << " found false after direction check\n";
         return false;
     }
 
@@ -306,7 +352,7 @@ bool Guard::atPossibleInfiniteLoop(Lab lab, char marker)
     // However the place we want to block may have already been taken by a past path
     // or be out of bounds as we are on the edge of the map already
     // So check this too
-    pair<int,int> blockingLoopPoint = getNextPos();
+    pair<int,int> blockingLoopPoint = getNextPos(1);
     int xBlocking = blockingLoopPoint.first;
     int yBlocking = blockingLoopPoint.second;
     if (xBlocking == -1 || xBlocking == lab.getSizeX()) {
@@ -319,13 +365,13 @@ bool Guard::atPossibleInfiniteLoop(Lab lab, char marker)
     }
 
     if (!possible) {
-        cout << "Requires point of obstacle " << getFileCoordinatesString(xBlocking, yBlocking) << " found false after out of bounds checks\n";
+        //cout << "Requires point of obstacle " << getFileCoordinatesString(xBlocking, yBlocking) << " found false after out of bounds checks\n";
         return false;
     }
     char nextChar = lab.getChar(xBlocking, yBlocking);
     if (nextChar == marker) {
         possible = false;
-        cout << "Requires point of obstacle " << getFileCoordinatesString(xBlocking, yBlocking) << " found false as previously travelled\n";
+        //cout << "Requires point of obstacle " << getFileCoordinatesString(xBlocking, yBlocking) << " found false as previously travelled\n";
     }
 
     return possible;
@@ -350,10 +396,16 @@ void Guard::setTravelDirectionOfPoint()
     // Set
     travelDirections[key] = dir;
 }
+
 Direction Guard::getTravelDirectionOfPoint()
 {
+    return getTravelDirectionOfPoint(xpos, ypos);
+}
+
+Direction Guard::getTravelDirectionOfPoint(int x, int y)
+{
     ostringstream os;
-    os << xpos << "x" << ypos;
+    os << x << "x" << y;
     string key = os.str();
 
     if (!travelDirections.contains(key))
@@ -366,3 +418,75 @@ Direction Guard::getTravelDirectionOfPoint()
     // Get
     return travelDirections[key];
 }
+
+bool runGuardInfiniteLoopCheck(Guard guard, Lab lab, int xPosObstacle, int yPosObstacle)
+{
+    // Here we run a subset of the P1 loop where we are only interested in
+    // tracking whether we enter an infinite loop or not
+    bool escaped = false;
+    pair<int,int> nextPos;
+    int xNext;
+    int yNext;
+    char nextChar;
+    char marker;
+    char notVisited = '.';
+    unordered_map<string,int> obstacleHits;
+    while (!escaped)
+    {
+        nextPos = guard.getNextPos(1);
+        xNext = nextPos.first;
+        yNext = nextPos.second;
+        // Are we at the edge
+        if (xNext == -1 || xNext == lab.getSizeX()) {
+            // Escaped horizontally
+            escaped = true;
+        }
+        else if (yNext == -1 || yNext == lab.getSizeY()) {
+            // Escaped vertically
+            escaped = true;
+        }
+        else {
+            // Process next step
+            nextChar = lab.getChar(xNext, yNext);
+            // We could also hit our new obstacle again!
+            if (nextChar == '#' || ( xPosObstacle == xNext && yPosObstacle == yNext )) {
+                // Hit an obstacle, so turn right instead
+                // First log it so we know which direction we hit from
+                ostringstream os;
+                string dirString;
+                switch (guard.dir)
+                {
+                    case UP:
+                        dirString = "up";
+                        break;
+                    case RIGHT:
+                        dirString = "ri";
+                        break;
+                    case DOWN:
+                        dirString = "do";
+                        break;
+                    case LEFT:
+                        dirString = "le";
+                        break;
+                    default:
+                        break;
+                }
+                os << xNext << "x" << yNext << dirString;
+                if (obstacleHits.contains(os.str()) && obstacleHits[os.str()] == 1) {
+                    return true;
+                }
+                else {
+                    obstacleHits[os.str()] = 1;
+                }
+                guard.turnRight();
+            }
+            else
+            {
+                // Move guard
+                guard.move();
+            }
+        }
+    }
+    return false;
+}
+
