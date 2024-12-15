8562bc58827289d60d2b1bc38db21cf8bf15c6c3
Simon Watts <14802483+siwatts@users.noreply.github.com>
2024-12-15 16:40:30 +0000

    Regex input parsing, for robots

--- day14/Program.cs
+++ day14/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System.Diagnostics;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace AOC
 {
+    public class Robot
+    {
+        public int px;
+        public int py;
+        public int vx;
+        public int vy;
+        public Robot(int px, int py, int vx, int vy)
+        {
+            this.px = px;
+            this.py = py;
+            this.vx = vx;
+            this.vy = vy;
+
+        }
+    }
     public class Program
     {
         public static int Main(string[] args)
         {
             Console.WriteLine("--\nAoC Day 14\n--");
             // For testing
-            int debuglimit = 10;
+            int debuglimit = 12;
             bool debugmode = false;
 
             // User args
@@ -47,12 +63,36 @@ namespace AOC
             // Read file
             String? line;
             int lineNr = 0;
+            List<Robot> robots = new List<Robot>();
             using (var streamReader = new StreamReader(filename))
             {
                 while ((line = streamReader.ReadLine()) != null && (!debugmode || lineNr < debuglimit))
                 {
-                    if (debugmode) {
+                    if (debugmode)
+                    {
                         Console.WriteLine(line);
+                    }
+
+                    // Parse robots
+                    var r = new Regex(@"p=(\S+),(\S+) v=(\S+),(\S+)");
+                    var m = r.Match(line);
+                    if (debugmode)
+                    {
+                        Console.WriteLine("Match groups '{0}', p={1},{2} v={3},{4}", m.Groups.Count, m.Groups[1].Value, m.Groups[2].Value, m.Groups[3].Value, m.Groups[4].Value);
+                    }
+                    if (m.Success && m.Groups.Count == 5)
+                    {
+                        // Messy regex parse method
+                        robots.Add(
+                            new Robot(
+                                int.Parse(m.Groups[1].Value),
+                                int.Parse(m.Groups[2].Value),
+                                int.Parse(m.Groups[3].Value),
+                                int.Parse(m.Groups[4].Value)));
+                    }
+                    else
+                    {
+                        throw new InvalidDataException("Fix regex input!");
                     }
 
                     lineNr++;
@@ -62,12 +102,13 @@ namespace AOC
             // Processing
             if (debugmode)
             {
+                Console.WriteLine("Made {0} robots", robots.Count);
             }
 
             // Output
             Console.WriteLine("--");
             Console.WriteLine("Sum = {0}", sum);
-            if (sum == 123)
+            if (sum == 12)
             {
                 Console.WriteLine("Answer matches example expected answer");
             }
